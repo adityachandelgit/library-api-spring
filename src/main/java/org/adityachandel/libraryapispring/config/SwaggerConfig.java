f6f5f388a45b4edb860b4874004ef7b58f360cc3
Aditya Chandel <aditya.chandel@motorolasolutions.com>
2020-12-10 18:15:45 -0700

    Add auth support in swagger

--- src/main/java/org/adityachandel/libraryapispring/config/SwaggerConfig.java
+++ src/main/java/org/adityachandel/libraryapispring/config/SwaggerConfig.java
@@ -6,42 +6,71 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.DocExpansion;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-  @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-      .useDefaultResponseMessages(false)
-      .select()
-      .apis(RequestHandlerSelectors.basePackage("org.adityachandel.libraryapispring.controller"))
-      .paths(PathSelectors.any())
-      .build()
-      .apiInfo(apiInfo());
-  }
+    public static final String AUTHORIZATION_HEADER = "Authorization";
 
-  @Bean
-  UiConfiguration uiConfig() {
-    return UiConfigurationBuilder.builder()
-            .docExpansion(DocExpansion.LIST)
-            .build();
-  }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.adityachandel.libraryapispring.controller"))
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
 
-  private ApiInfo apiInfo() {
-    return new ApiInfoBuilder()
-      .title("Library API")
-      .description("Library API is used for managing the books in the library. " +
-              "All API endpoints require a valid Basic token in the 'Authorization' header.")
-      .version("1.0.0")
-      .build();
-  }
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .docExpansion(DocExpansion.LIST)
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("Library API is used for managing the books in the library. " +
+                        "All API endpoints require a valid Basic token in the 'Authorization' header.")
+                .version("1.0.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
 
 }
